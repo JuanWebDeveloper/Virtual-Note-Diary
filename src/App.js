3f0b56d81bef2973c11ebbabc8883f3b998996f4
Juan Vargas <juandeveloper19@gmail.com>
2021-12-14 09:59:08 -0500

    Feat: LoginView component content created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { AppRouter } from './routers/AppRouter';
 
 // App Styles
-import './styles/styles.scss';
+import './assets/styles/styles.scss';
 
 export const App = () => {
 	return <AppRouter />;
--- src/components/auth/LoginView.js
+++ src/components/auth/LoginView.js
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom';
+
+import googleLogo from '../../assets/images/auth/google_logo.svg';
+
 export const LoginView = () => {
 	return (
-		<div>
-			<h1>Login View</h1>
+		<div className='auth__login-main'>
+			<div className='auth__login-main-content'>
+				<h2 className='title'>Login</h2>
+				<form>
+					<input type='text' name='email' placeholder='Email' className='auth__form-input' autoComplete='off' />
+					<input type='password' name='password' placeholder='Password' className='auth__form-input' />
+					<button className='auth__form-button'>Login</button>
+
+					<div className='auth__social-networks'>
+						<h4 className='sub-title'>Or login with</h4>
+						<div className='auth__google-button'>
+							<div className='auth__google-logo'>
+								<img src={googleLogo} alt='google button' />
+							</div>
+							<p className='auth__google-button-text'>
+								<b>Sign in with google</b>
+							</p>
+						</div>
+					</div>
+
+					<Link to='' className='button'>
+						Create new account
+					</Link>
+				</form>
+			</div>
 		</div>
 	);
 };
