9eedd5869ec2eca748aba16ab769f851a311ca88
Juan Vargas <juandeveloper19@gmail.com>
2021-12-14 06:51:48 -0500

    Feat: HomePage component content created

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export const HomePage = () => {
 	return (
-		<div>
-			<h1 className='title'>Home Page</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, deleniti?</p>
+		<div className='home__main'>
+			<div className='home__main-content'>
+				<h1 className='title'>Welcome Juan</h1>
+				<p>You have 10 notes created</p>
+				<Link to='/' className='button button-primary'>
+					View Notes
+				</Link>
+			</div>
 		</div>
 	);
 };
