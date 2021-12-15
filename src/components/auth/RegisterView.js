db7823079de0f2b422ca40fb35b3136447d77d09
Juan Vargas <juandeveloper19@gmail.com>
2021-12-15 13:49:46 -0500

    Feat: The hook was implemented to handle the form of the RegisterView component

--- src/components/auth/RegisterView.js
+++ src/components/auth/RegisterView.js
@@ -1,15 +1,39 @@
 import { Link } from 'react-router-dom';
 
+import { useForm } from '../../hooks/useForm';
+
 export const RegisterView = () => {
+	const [formValues, handleInputChange] = useForm({
+		name: '',
+		email: '',
+		password: '',
+		passwordConfirm: '',
+	});
+
+	const { name, email, password, passwordConfirm } = formValues;
+
+	const handleRegister = (e) => {
+		e.preventDefault();
+
+		console.log(name, email, password, passwordConfirm);
+	};
+
 	return (
 		<div className='auth__register-main'>
 			<div className='auth__register-main-content'>
 				<h2 className='title'>Register</h2>
-				<form className='auth__form'>
-					<input type='text' name='name' placeholder='Name' className='auth__form-input' autoComplete='off' />
-					<input type='text' name='email' placeholder='Email' className='auth__form-input' autoComplete='off' />
-					<input type='password' name='password' placeholder='Password' className='auth__form-input' />
-					<input type='password' name='passwordConfirm' placeholder='Confirm password' className='auth__form-input' />
+				<form className='auth__form' onSubmit={handleRegister}>
+					<input type='text' name='name' placeholder='Name' className='auth__form-input' autoComplete='off' value={name} onChange={handleInputChange} />
+					<input type='text' name='email' placeholder='Email' className='auth__form-input' autoComplete='off' value={email} onChange={handleInputChange} />
+					<input type='password' name='password' placeholder='Password' className='auth__form-input' value={password} onChange={handleInputChange} />
+					<input
+						type='password'
+						name='passwordConfirm'
+						placeholder='Confirm password'
+						className='auth__form-input'
+						value={passwordConfirm}
+						onChange={handleInputChange}
+					/>
 					<button className='auth__form-button'>Register</button>
 
 					<Link to='/auth/login' className='auth__link'>
