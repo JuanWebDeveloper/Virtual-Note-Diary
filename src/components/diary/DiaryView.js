9563967d5fd8e3c98ab3a42ee7eb05d376ab6731
Juan Vargas <juandeveloper19@gmail.com>
2021-12-15 01:21:05 -0500

    Feat: The component and the path for the diary was created

---
+++ src/components/diary/DiaryView.js
@@ -0,0 +1,7 @@
+export const DiaryView = () => {
+	return (
+		<div>
+			<h2>Diary View</h2>
+		</div>
+	);
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import { AuthRouter } from './AuthRouter';
 import { HomePage } from '../components/home/HomePage';
+import { DiaryView } from '../components/diary/DiaryView';
 
 export const AppRouter = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route exact path='/' element={<HomePage />} />
+				<Route exact path='/diary/' element={<DiaryView />} />
 				<Route path='/auth/*' element={<AuthRouter />} />
 				<Route path='*' element={<Navigate to='/auth/login' />} />
 			</Routes>
