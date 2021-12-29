7dcfa3fba503f81e07e6015aef680347540ee0d6
Juan Vargas <juandeveloper19@gmail.com>
2021-12-29 02:09:30 -0500

    Feat: The action to identify the active note was created

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,17 +1,23 @@
 import { firestore, addDoc, collection } from '../firebase/firebaseConfig';
+import { types } from '../types/types';
 
 export const actionAddNewNote = () => {
 	return async (dispatch, getState) => {
 		const { uid } = getState().auth;
 
 		const newNote = {
-			title: '',
-			description: '',
+			title: 'Hello',
+			description: 'Hello World',
 			createdAt: new Date().toISOString(),
 		};
 
 		const documentReferenceCreated = await addDoc(collection(firestore, `${uid}/diary/notes`), newNote);
 
-		console.log(documentReferenceCreated);
+		dispatch(actionNoteActive(documentReferenceCreated.id, newNote));
 	};
 };
+
+export const actionNoteActive = (noteId, note) => ({
+	type: types.noteActive,
+	payload: { noteId, ...note },
+});
