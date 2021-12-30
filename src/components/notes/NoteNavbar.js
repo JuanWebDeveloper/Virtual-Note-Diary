51a25c176c02d697ce95807bd68bb5ac3660516b
Juan Vargas <juandeveloper19@gmail.com>
2021-12-29 21:49:25 -0500

    Feat: The option to select an image was enabled by clicking on the picture button

--- src/components/notes/NoteNavbar.js
+++ src/components/notes/NoteNavbar.js
@@ -9,12 +9,21 @@ export const NoteNavbar = () => {
 		dispatch(actionSaveChanges());
 	};
 
+	const handleSelectedFile = () => document.getElementById('selectedFile').click();
+
+	const handleSelectedFileChange = (e) => {
+		const file = e.target.files[0];
+
+		console.log(file);
+	};
+
 	return (
 		<div className='notes__note-navbar'>
 			<h3>28/10/21</h3>
+			<input type='file' style={{ display: 'none' }} id='selectedFile' onChange={handleSelectedFileChange} />
 
 			<div className='notes__note-navbar-buttons'>
-				<button>Picture</button>
+				<button onClick={handleSelectedFile}>Picture</button>
 				<button onClick={handleSaveChanges}>Save</button>
 			</div>
 		</div>
