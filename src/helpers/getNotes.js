d9c40e3c08ab001b1a7adfeba4261d9a49d806e6
Juan Vargas <juandeveloper19@gmail.com>
2021-12-31 01:32:36 -0500

    Fix: User notes are now sorted according to their creation date

--- src/helpers/getNotes.js
+++ src/helpers/getNotes.js
@@ -1,7 +1,11 @@
+import { query, orderBy } from 'firebase/firestore';
 import { firestore, collection, getDocs } from '../firebase/firebaseConfig';
 
 export const getNotes = async (uid) => {
-	const snapshot = await getDocs(collection(firestore, `${uid}/diary/notes`));
+	const notesRef = collection(firestore, `${uid}/diary/notes`);
+	const q = query(notesRef, orderBy('createdAt', 'desc'));
+	const snapshot = await getDocs(q);
+
 	const userNotes = [];
 
 	snapshot.forEach((note) => {
